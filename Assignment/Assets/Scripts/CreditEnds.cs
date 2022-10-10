f613533d08bbfbcbe54600f1976ad921a5bfcb63
Spaguette1 <79942474+Spaguette1@users.noreply.github.com>
2022-10-11 01:56:11 +1000

    Credit Scene Addition
    Finished the credit screen

---
+++ Assignment/Assets/Scripts/CreditEnds.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditEnds : MonoBehaviour
+{
+
+    public GameObject EndScript;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        StartCoroutine(waiter());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds (31);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
--- Assignment/Assets/Scripts/MainMenu.cs
+++ Assignment/Assets/Scripts/MainMenu.cs
@@ -29,5 +29,10 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 
+    public void CreditScreen()
+    {
+        SceneManager.LoadScene("CreditScreen");
+    }
+
 
 }
